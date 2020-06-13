bc29fc42fce192415a2c296e3ded3703ee347229
xomidar <hello@rezaulhasan.me>
2020-06-13 10:46:32 +0600

    6.1 array

--- src/me/rezaulhasan/Main.java
+++ src/me/rezaulhasan/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        long viewsCounts8 = 5_123_456_789L;
-        System.out.println(viewsCounts8);
+        int[] employeeIds = new int[310];
 
-        int viewsCounts4 = (int)viewsCounts8;
-        System.out.println(viewsCounts4);
+        employeeIds[0] = 1;
+
+        System.out.println(employeeIds);
 
     }
 }
