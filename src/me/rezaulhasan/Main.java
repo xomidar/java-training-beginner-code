25838c4f8ada80f4fdbc2f0678bb667bc845b685
xomidar <hello@rezaulhasan.me>
2020-06-13 11:00:38 +0600

    7.1 constant

--- src/me/rezaulhasan/Main.java
+++ src/me/rezaulhasan/Main.java
@@ -6,12 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[][] numbers = new int[2][3];
 
-        numbers[0][0] =  1;
+        final int workHour = 8;
+        final int lunchHour = 1;
 
-        System.out.println(numbers.length);
+        int officeHour = workHour + lunchHour;
 
-        System.out.println(Arrays.deepToString(numbers));
+        System.out.println(officeHour);
+
     }
 }
