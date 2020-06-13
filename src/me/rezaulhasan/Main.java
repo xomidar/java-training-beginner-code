d7c6ef4fd5c997cc2018ac2abced1024365a0f1e
xomidar <hello@rezaulhasan.me>
2020-06-13 10:36:54 +0600

    4.1 primitive type init

--- src/me/rezaulhasan/Main.java
+++ src/me/rezaulhasan/Main.java
@@ -4,14 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int riceDish = 30;
+        byte age1 = 30;
+        short employeeCount2 = 200;
+        int viewsCounts4 = 123_456_789;
+        long viewsCounts8 = 5_123_456_789L;
 
-        int ricePlate;
-
-        ricePlate = riceDish;
-
-        System.out.println(riceDish);
-
-        System.out.println(ricePlate);
+        System.out.println(viewsCounts4);
     }
 }
