8192ece6fd8623d38456710d298c23fce1b46709
xomidar <hello@rezaulhasan.me>
2020-06-13 11:15:58 +0600

    10.2 If else start

--- src/me/rezaulhasan/Main.java
+++ src/me/rezaulhasan/Main.java
@@ -4,11 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int myAge = 20;
+        int temperature = 21;
 
-        int herAge = 18;
+        boolean isWarm = temperature > 30;
 
+        System.out.println(isWarm);
 
-        System.out.println(myAge == herAge);
+        boolean isNormal = temperature > 20 && temperature <= 30;
+
+        System.out.println(isNormal);
+
+        boolean isCold = temperature < 20;
+
+        System.out.println(isCold);
+
     }
 }
