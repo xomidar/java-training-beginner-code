2892b7ff7108187d90847e9584c756ca0e9dbe31
xomidar <hello@rezaulhasan.me>
2020-06-20 10:50:33 +0600

    3.3 Java 13 switch expression

--- src/me/rezaulhasan/Main.java
+++ src/me/rezaulhasan/Main.java
@@ -8,37 +8,37 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("What is today's temperature: ");
-        int temperature = scanner.nextInt();
-
-        boolean isWarm = temperature > 30;
-        boolean isCold = temperature < 20;
+/*
+        System.out.print("Your role: ");
+        String role = scanner.next();
 
 
-        if (isWarm) {
-
-            System.out.println("It's very hot!");
-
-        } else if (isCold) {
-
-            System.out.println("It's very cold!");
-
-        } else {
-
-            System.out.println("It's good day.");
-
-        }
-
-        switch (true) {
-            case isWarm:
+        switch (role) {
+            case "admin":
                 System.out.println("Your are admin");
                 break;
-            case isCold:
+            case "user":
                 System.out.println("Your are user");
                 break;
             default:
                 System.out.println("Your are unknown");
+
         }
+*/
+
+        System.out.print("What is today's temperature: ");
+        int temperature = scanner.nextInt();
+
+
+        var result = switch (temperature) {
+            case 3, 4, 5 -> "Spring";
+            case 6, 7, 8 -> "Summer";
+            case 9, 10, 11 -> "Fall";
+            case 12, 1, 2 -> "Winter";
+            default -> "None";
+        };
+
+        System.out.println(result);
 
     }
 }
