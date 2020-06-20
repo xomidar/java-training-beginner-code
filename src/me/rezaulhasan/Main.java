3233a1d41e5cb88f99463d55dee6f7573d8fe16b
xomidar <hello@rezaulhasan.me>
2020-06-20 11:01:44 +0600

    4.4 Forloop array

--- src/me/rezaulhasan/Main.java
+++ src/me/rezaulhasan/Main.java
@@ -5,21 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
 
-        for (int i = 0; i < 9; i++) {
+        int[] employeeIds = {10, 20, 30, 40, 50};
 
-            System.out.println("Hello World " + i);
-        }
+        for (int i = 0; i < employeeIds.length; i++) {
 
-        for (int i = 1; i <= 9; i++) {
-
-            System.out.println("Hello World " + i);
-
-        }
-
-
-        for (int i = 9; i > 0; i--) {
-
-            System.out.println("Hello World" + i);
+            System.out.println("Employee ID: " + employeeIds[i]);
 
         }
 
