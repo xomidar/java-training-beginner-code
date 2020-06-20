54fa74d07a52ca703bf7379e3b907c9fc41be12d
xomidar <hello@rezaulhasan.me>
2020-06-20 10:28:07 +0600

    2.4 If else condition hierarchy example

--- src/me/rezaulhasan/Main.java
+++ src/me/rezaulhasan/Main.java
@@ -13,9 +13,10 @@ public class Main {
 
         boolean isWarm = temperature > 30;
         boolean isCold = temperature < 20;
-        boolean isNormal = temperature > 20 && temperature <= 30;
+//        boolean isNormal = temperature > 20 && temperature <= 30;
 
 
+/*
         if (isWarm) {
 
             System.out.println("It's very hot!");
@@ -29,5 +30,21 @@ public class Main {
             System.out.println("It's good day.");
 
         }
+*/
+
+        if (isWarm) {
+
+            System.out.println("It's very hot!");
+
+        } else if (isCold) {
+
+            System.out.println("It's very cold!");
+
+        } else {
+
+            System.out.println("It's good day.");
+
+        }
+
     }
 }
