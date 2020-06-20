531ca7fe7e4713d2f10449aa95815743acfecc5d
xomidar <hello@rezaulhasan.me>
2020-06-20 10:20:31 +0600

    2.1 If else Basic

--- src/me/rezaulhasan/Main.java
+++ src/me/rezaulhasan/Main.java
@@ -1,8 +1,26 @@
 package me.rezaulhasan;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is today's temperature: ");
+        int temperature = scanner.nextInt();
+
+        boolean isWarm = temperature > 30;
+
+        if (isWarm) {
+
+            System.out.println("It's hot!");
+
+        } else {
+
+            System.out.println("It's cold!");
+
+        }
     }
 }
