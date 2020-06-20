89c581038e45f8a7c4b740574bd03090eff78d63
xomidar <hello@rezaulhasan.me>
2020-06-20 11:04:36 +0600

    6.1 While loop

--- src/me/rezaulhasan/Main.java
+++ src/me/rezaulhasan/Main.java
@@ -5,12 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int[] employeeIds = {10, 20, 30, 40, 50};
+        for (int i = 9; i > 0; i--) {
 
-        for (int employeeId : employeeIds) {
+            System.out.println("Hello World " + i);
 
-            System.out.println("Employee ID: " + employeeId);
+        }
 
+        int i = 9;
+
+        while(i > 0){
+
+            System.out.println("Hello World " + i);
+            i--;
         }
 
     }
